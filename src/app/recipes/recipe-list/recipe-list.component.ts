74caa409a1e1d6c04a18e838f30a0988f917515c
Mariusz Trocewicz <mariusz.trocewicz@radrat.pl>
2024-05-05 01:17:26 +0200

    recipe list wip

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -20,4 +20,18 @@ export class RecipeListComponent {
       'https://images.unsplash.com/photo-1481070555726-e2fe8357725c?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
     ),
   ];
+  //   recipes: Recipe[] = [
+  //     {
+  //       name: 'Avocado and Egg Toast',
+  //       description: 'Health & Wellness',
+  //       imageSrc:
+  //         'https://images.unsplash.com/photo-1482049016688-2d3e1b311543?q=80&w=1910&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  //     },
+  //     {
+  //       name: 'Chicken Sandwich',
+  //       description: 'Food & Drink',
+  //       imageSrc:
+  //         'https://images.unsplash.com/photo-1481070555726-e2fe8357725c?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  //     },
+  //   ];
 }
